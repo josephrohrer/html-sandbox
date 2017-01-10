17cde995cf52f89c3830910e2001fdf2ffb8abe6
Joseph Rohrer <josephrohrer@me.com>
2017-01-10 15:46:04 -0700

    Set up bare bones HTML structure

--- index.php
+++ index.php
@@ -5,9 +5,48 @@
 		<title>HTML SANDBOX</title>
 	</head>
 	<body>
-		<h1>This is a Demo page.</h1>
-		<p>I'll alert the crew. We know you're dealing in stolen ore. But I wanna talk about the assassination attempt on Lieutenant Worf. You enjoyed that. Sorry, Data. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'? Maybe we better talk out here; the observation lounge has turned into a swamp. Worf, It's better than music. It's jazz. For an android with no feelings, he sure managed to evoke them in others. We could cause a diplomatic crisis. Take the ship into the Neutral Zone Commander William Riker of the Starship Enterprise. That might've been one of the shortest assignments in the history of Starfleet.</p>
-		<h2>This is a header two.</h2>
-		<h3>This is a header three.</h3>
+		<header>
+			<h1>This is a Demo page.</h1>
+		</header>
+		<main>
+			<p>I'll alert the crew. We know you're dealing in stolen ore. But I wanna talk about the assassination attempt on Lieutenant Worf. You enjoyed that. Sorry, Data. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'? Maybe we better talk out here; the observation lounge has turned into a swamp. Worf, It's better than music. It's jazz. For an android with no feelings, he sure managed to evoke them in others. We could cause a diplomatic crisis. Take the ship into the Neutral Zone Commander William Riker of the Starship Enterprise. That might've been one of the shortest assignments in the history of Starfleet.</p>
+			<!-- This is a comment -->
+		</main>
+			<h2>This is a header two.</h2>
+			<ul>
+				<li>Cats</li>
+				<li>Bowling</li>
+				<li>Tacos</li>
+				<li>Tow Trucks</li>
+			</ul>
+
+			<h3>This is a table with header three.</h3>
+			<table>
+				<!-- Row 1 -->
+				<tr>
+					<th>Column 1</th>
+					<th>Column 2</th>
+				</tr>
+
+				<!-- Row 2 -->
+				<tr>
+					<td>Tabby</td>
+					<td>Beagle</td>
+				</tr>
+
+				<!-- Row 3 -->
+				<tr>
+					<td>Persian</td>
+					<td>Bulldog</td>
+				</tr>
+
+				<!-- Row 4 -->
+				<tr>
+					<td>Siamese</td>
+					<td>Boxer</td>
+				</tr>
+			</table>
+
+			<img src="images/catdog.png" alt="Catdog">
 	</body>
 </html>
