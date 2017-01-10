ebf93bac189fe38364872b1950a190eea7b4ec46
Joseph Rohrer <josephrohrer@me.com>
2017-01-10 15:16:59 -0700

    Set up bare bones HTML structure

--- index.php
+++ index.php
@@ -4,5 +4,10 @@
 		<meta charset="utf-8" />
 		<title>HTML SANDBOX</title>
 	</head>
-	<body></body>
+	<body>
+		<h1>This is a Demo page.</h1>
+		<p>I'll alert the crew. We know you're dealing in stolen ore. But I wanna talk about the assassination attempt on Lieutenant Worf. You enjoyed that. Sorry, Data. Earl Grey tea, watercress sandwiches... and Bularian canapés? Are you up for promotion? Why don't we just give everybody a promotion and call it a night - 'Commander'? Maybe we better talk out here; the observation lounge has turned into a swamp. Worf, It's better than music. It's jazz. For an android with no feelings, he sure managed to evoke them in others. We could cause a diplomatic crisis. Take the ship into the Neutral Zone Commander William Riker of the Starship Enterprise. That might've been one of the shortest assignments in the history of Starfleet.</p>
+		<h2>This is a header two.</h2>
+		<h3>This is a header three.</h3>
+	</body>
 </html>
